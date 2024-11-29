a0de475dbee3655bb4f9cb82619f17f53c15f23a
Bilelbourgou <bilelbourgou@gmail.com>
2024-11-29 22:33:55 +0100

    add job dynamic job details page

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -2,6 +2,9 @@
 import JobListing from '@/components/jobListing.vue';
 import { reactive,defineProps, onMounted } from 'vue';
 import { RouterLink } from 'vue-router';
+import PulseLoader from "vue-spinner/src/PulseLoader.vue"
+
+
 import axios from 'axios';
 
 defineProps({
@@ -35,8 +38,14 @@ onMounted(async () =>{
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
                 Browse Jobs
             </h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in state.jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+            <!-- Show loading spinner while loading is true -->
+            <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader/>
+            </div>
+
+            <!-- Show job listings when done loading -->
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
             </div>
         </div>
     </section>
