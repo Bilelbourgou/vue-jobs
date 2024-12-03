a3ad2513ff024d4f38536e22c1544441603b01ba
Bilelbourgou <bilelbourgou@gmail.com>
2024-12-03 20:29:57 +0100

    add toast notification and delete functionality

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import './assets/main.css'
-import 'primeicons/primeicons.css'
-import router from './router'
-import { createApp } from 'vue'
-import App from './App.vue'
+import './assets/main.css';
+import 'primeicons/primeicons.css';
+import 'vue-toastification/dist/index.css';
+import Toast from 'vue-toastification';
+import router from './router';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(Toast);
+app.mount('#app');
